d28ef389151ba2afe5523853bd38367510ad0c08
Lucas75Soares <118849263+Lucas75Soares@users.noreply.github.com>
2023-03-24 14:39:49 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -11,8 +11,27 @@ function calcular(){
     var retorno = valorInvestido.value * taxaReal;
     var montante = Number(retorno) + Number(valorInvestido.value);
     var montanteRetorno = document.getElementById("montante-retorno");
-    retornoElemento.innerText = "retorno: R$ "+ retorno;
-    montanteRetorno.innerText = "montante: R$"+ montante;
+    montanteRetorno.style.display="block";
+    retornoElemento.style.display="block";
+    retornoElemento.innerText = "Retorno: R$ "+ retorno.toFixed(2);
+    montanteRetorno.innerText = "Montante: R$"+ montante.toFixed(2);
 
     console.log(retorno)
 }
+
+const audio = new Audio('money.mp3')
+audio.play()
+
+var somLogo = document.getElementById("logo")
+var clicked = true;
+
+somLogo.addEventListener("click", ()=> {
+    if (clicked == true) {
+        audio.play()
+        alert("Faça seu investiment!")
+        clicked = false;
+    } else {
+        audio.pause()
+        clicked = true;
+    }
+})
